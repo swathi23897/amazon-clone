21a0b8f85d6d855ad2ab6786542cf67c380a3e26
swathi23897 <swaa2397@gmail.com>
2021-01-03 23:23:32 +0530

    complete

--- app.js
+++ app.js
@@ -40,9 +40,9 @@ app.post("/payments/create", async (request, response) => {
 });
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(__dirname,'build'));
+    app.use(express.static('frontend/build'));
     app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname,'','build','index.html'));
+        res.sendFile(path.join(__dirname,'frontend','build','index.html'));
     })
 
 
